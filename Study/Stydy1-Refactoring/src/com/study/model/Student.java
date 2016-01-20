b0f3b30aa7e91bdc113efe1690440f33ab3ffcfb
simjs <sim.jaesik@linecorp.com>
2016-01-20 17:28:48 +0900

    Request 1-3

---
+++ Study/Stydy1-Refactoring/src/com/study/model/Student.java
@@ -0,0 +1,25 @@
+package com.study.model;
+
+public class Student {
+	
+	private String studentNo = "";
+	private int studentJumsu = 0;
+	
+	
+	public String getStudentNo() {
+		return studentNo;
+	}
+	public void setStudentNo(String studentNo) {
+		this.studentNo = studentNo;
+	}
+	public int getStudentJumsu() {
+		return studentJumsu;
+	}
+	public void setStudentJumsu(int studentJumsu) {
+		this.studentJumsu = studentJumsu;
+	}
+	
+	
+	
+
+}
---
+++ Study/Stydy1-Refactoring/src/com/study/model/accesser/DataWriter.java
@@ -0,0 +1,9 @@
+package com.study.model.accesser;
+
+import java.util.List;
+
+import com.study.model.Student;
+
+public interface DataWriter {
+	public void writeData(List<Student> data);
+}
